c2bdf66126a1234323bc16a082e9dfb4cdc4915c
Mitchelle Makini <mitchellemakini15@gmail.com>
2019-03-03 19:03:28 +0300

    created Player object
    created player object with 2 methods: roll & hold

--- js/script.js
+++ js/script.js
@@ -1,3 +1,35 @@
+function Player(name,winscore){
+	this.name=name;
+	this.totalScore=0;
+	this.sessionScore=0;
+	this.winscore=winscore;
+}
+
+// roll method
+Player.prototype.roll= function(){
+
+	var rolledDice=Math.floor(Math.random()*6+1);
+	if(rolledDice>1){
+		this.sessionScore+=rolledDice;
+		if((this.sessionScore+this.totalScore)>=this.winscore){
+            // declare user as winner
+			return 0;
+		}
+	}
+	else{
+		this.sessionScore=0;
+	}
+	return rolledDice;
+}
+
+// hold method
+Player.prototype.hold=function(){
+	this.totalScore+=this.sessionScore;
+	this.sessionScore=0;
+	return this.totalScore;
+}
+
+
 $(document).ready(
     function(){
         $(".game-play").hide();
